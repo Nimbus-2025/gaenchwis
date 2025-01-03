3d37891ddf45a88ef2c784f0c75fe5ad339ad594
YunJae Park <zerglisk123@naver.com>
2025-01-03 17:20:48 +0900

    feat: 텍스트 드래그 우클릭 저장 ver0.1

---
+++ chrome_extension/src/module/Drag.js
@@ -0,0 +1,37 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.get(null, (result) => {
+    const essay = result.essay || {};
+    const essay_titles = Object.keys(essay)
+    const new_essay_title_num = essay_titles.length+1;
+
+    chrome.contextMenus.create({
+      id: "dragTitle",
+      title: new_essay_title_num+"번 자기소개서 문항 저장",
+      contexts: ["selection"]
+    });
+    for (let i=1; i<new_essay_title_num; i++){
+      if (essay[essay_titles[i]] === null){
+        chrome.contextMenus.create({
+          id: i,
+          title: i+"번 자기소개서 내용 저장",
+          contexts: ["selection"]
+        });
+      }
+    }
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: (type, text) => {
+      chrome.storage.local.get(null, (result) => {
+        const essay = result.essay || {};
+        if (type === "dragTitle") {
+          essay[text]=null
+        }
+      });
+    },
+    args: [info.menuItemId,info.selectionText]
+  });
+});
