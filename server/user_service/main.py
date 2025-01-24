cc2ef8eacf287f673185631fd2d54e93e286632b
sookyoung <topgdvidsyb@naver.com>
2025-01-24 10:26:44 +0900

    fix: user_service 포트 변경

--- server/user_service/main.py
+++ server/user_service/main.py
@@ -95,4 +95,4 @@ app.include_router(router, prefix="/api/v1", tags=["user"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app",  host="0.0.0.0", port=8000, reload=True, log_level="info")
+    uvicorn.run("main:app",  host="0.0.0.0", port=8005, reload=True, log_level="info")
