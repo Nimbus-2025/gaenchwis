0267958ed59dbf2c85c09fab816dd607dd3c14f6
sookyoung <topgdvidsyb@naver.com>
2025-01-26 23:04:33 +0900

    feat: job posting 파이프라인 구성 파일 생성

--- server/job_posting/fetch_jobs.py
+++ server/job_posting/fetch_jobs.py
@@ -11,15 +11,15 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+# aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+# aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 aws_region = os.getenv('AWS_REGION')
 
 dynamodb = boto3.resource(
     'dynamodb',
     region_name=aws_region,
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key
+    # aws_access_key_id=aws_access_key_id,
+    # aws_secret_access_key=aws_secret_access_key
 )
 
 def get_tag_names(job_tags, dynamodb):
