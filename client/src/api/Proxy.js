bca6579bee3b4e2ec7b1149e0ec4d2ba09bb516f
sookyoung <topgdvidsyb@naver.com>
2025-01-28 22:20:38 +0900

    fix: 크롤러 로그그룹 수정

--- client/src/api/Proxy.js
+++ client/src/api/Proxy.js
@@ -1,7 +1,8 @@
 const Proxy = {
-  server: "https://alb.gaenchwis.click",
-  callback: "https://gaenchwis.click/callback",
-  domain: "https://gaenchwis.auth.ap-northeast-2.amazoncognito.com"
-}
+  server: 'https://alb.gaenchwis.click',
+  // callback: "https://gaenchwis.click/callback",
+  callback: 'http://localhost:3000/callback',
+  domain: 'https://gaenchwis.auth.ap-northeast-2.amazoncognito.com',
+};
 
 export default Proxy;
